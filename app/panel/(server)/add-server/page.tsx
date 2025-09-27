c6e57658caa6b6026dfae02fb9f3b5820fdd97bb
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-09-27 16:42:48 +0700

    add padding in add server page

--- app/panel/(server)/add-server/page.tsx
+++ app/panel/(server)/add-server/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function AddServerPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen container space-y-6">
+    <div className="flex flex-col items-center justify-center h-screen container space-y-6 py-10">
       {/* Title */}
       <h1 className="text-4xl font-semibold text-center">สร้าง Server</h1>
       <hr className="border-gray-300 w-full max-w-lg" />
