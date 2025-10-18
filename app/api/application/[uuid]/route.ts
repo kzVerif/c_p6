776a6c42436e9337e6f10a91b1cee7752001e7d3
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-10-18 12:46:05 +0700

    test res type

--- app/api/application/[uuid]/route.ts
+++ app/api/application/[uuid]/route.ts
@@ -1,4 +1,4 @@
-export async function GET(req: Request, {params} : {params: {uuid: string}}) {
+export async function GET(req: Request, res: Response, {params} : {params: {uuid: string}}) {
     try {
         const {uuid} = await params;
         const url = `${process.env.COOLIFY_URL}/api/v1/applications/${uuid}`
