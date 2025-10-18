825854061145c440f40a870ee23799984322ec67
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-10-18 12:55:22 +0700

    response app uuid

--- app/api/application/[uuid]/route.ts
+++ app/api/application/[uuid]/route.ts
@@ -1,8 +1,12 @@
+import { NextResponse } from "next/server";
+
 export async function GET(req: Request, res: Response, {params} : {params: {uuid: string}}) {
     try {
         const {uuid} = await params;
         const url = `${process.env.COOLIFY_URL}/api/v1/applications/${uuid}`
         const response = await fetch(url)
+        const data = await response.json()
+        return NextResponse.json({status:200,data: data})
     } catch (error) {
         
     }
